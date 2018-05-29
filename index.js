1ebab6fe755f67ef1b79aafd3c10a18ceb1ac9f9
Ajit Kishor <t-ajkish@microsoft.com>
2018-05-29 17:55:31 +0530

    "commit"

--- index.js
+++ index.js
@@ -10,33 +10,34 @@ server.use(bodyParser.urlencoded({
 
 server.use(bodyParser.json());
 
-server.post('https://movieboot.herokuapp.com/webhook', (req, res) => {
-    
-        const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'The Godfather';
-        const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
-        http.get(reqUrl, (responseFromAPI) => {
-            let completeResponse = '';
-            responseFromAPI.on('data', (chunk) => {
-                completeResponse += chunk;
-            });
-            responseFromAPI.on('end', () => {
-                const movie = JSON.parse(completeResponse);
-                let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
-                dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
-    
-                return res.json({
-                    speech: dataToSend,
-                    displayText: dataToSend,
-                    source: 'webhook'
-                });
-            });
-        }, (error) => {
+server.post('/get-movie-details', (req, res) => {
+
+    const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
+    const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
+    http.get(reqUrl, (responseFromAPI) => {
+        let completeResponse = '';
+        responseFromAPI.on('data', (chunk) => {
+            completeResponse += chunk;
+        });
+        responseFromAPI.on('end', () => {
+            const movie = JSON.parse(completeResponse);
+            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
+            dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
+
             return res.json({
-                speech: 'Something went wrong!',
-                displayText: 'Something went wrong!',
-                source: 'webhook'
+                speech: dataToSend,
+                displayText: dataToSend,
+                source: 'get-movie-details'
             });
         });
+    }, (error) => {
+        return res.json({
+            speech: 'Something went wrong!',
+            displayText: 'Something went wrong!',
+            source: 'get-movie-details'
+        });
     });
-
- 
+});
+    server.listen((process.env.PORT || 8000), () => {
+        console.log("Server is up and running...");
+    });
